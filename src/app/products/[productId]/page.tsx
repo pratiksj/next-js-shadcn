4cf100ce89bcdd4f4e662d9981e5771be28d7c01
pratiksj <pratiksharai728@gmail.com>
2024-10-25 16:37:51 +0545

    nested dynamic route

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,16 @@
+export default function ProductDetails ({params}:{
+    params:{
+        productId:string
+    }
+}){
+
+
+
+    return (
+        <>
+        
+        <h1>Details about product{params.productId}</h1>
+       
+        </>
+    )
+} 
---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+export default function ProductList(){
+
+
+    return (
+        <>
+        
+        <h1>product list</h1>
+        <h2>product list</h2>
+        <h3>product list</h3>
+        <h4>product list</h4>
+        </>
+    )
+}
