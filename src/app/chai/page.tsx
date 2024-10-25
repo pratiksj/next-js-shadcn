b0cc37d61b59c58b58db1d3ee4af60f46e9ac53d
pratiksj <pratiksharai728@gmail.com>
2024-10-25 15:14:21 +0545

    add global css

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,9 @@
+function ChaiPage(){
+    return (
+       <main  className="h-full flex justify-center items-center">
+         <div>
+            ChaiPage
+        </div>
+       </main>
+        )
+}
